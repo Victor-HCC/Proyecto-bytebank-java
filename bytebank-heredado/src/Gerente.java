ff08e287a7bd31eff27fb284da5f48a3c5ddfb4c
Victor <vicktorcori@hotmail.com>
2022-08-28 21:07:11 -0300

    Practicando polimorfismo

--- bytebank-heredado/src/Gerente.java
+++ bytebank-heredado/src/Gerente.java
@@ -10,4 +10,11 @@ public class Gerente extends Funcionario { //Gerente hereda de Funcionario
 	public boolean iniciarSesion(String clave) {
 		return clave == "Alura";
 	}
+	
+	//Sobre-escritura de metodos, la visivilidad, tipo de retorno, nombre y parametros deben ser iguales
+	public double getBonificacion() {
+		return super.getSalario() + 
+				//(super.getSalario() * 0.1) //Como se le agrega el 10% del salario puedo usar el metodo ya definido en Funcionario
+				super.getBonificacion(); //con super accedemos a los metodos de la clase padre
+	}
 }
